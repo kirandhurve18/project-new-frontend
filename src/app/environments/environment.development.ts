0f20447201cd48dabcd68a01ec5d160954b882d4
kirandhurve18 <kirand1895@gmail.com>
2026-01-29 16:36:15 +0530

    Update environment.development.ts

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://api.desteksolutions.com/api',
-  apiPrefix: 'https://api.desteksolutions.com/api',
-  BASE_URL_IMAGE: 'https://api.desteksolutions.com/api',
-  BASE_URL_DOCUMENTS: 'https://api.desteksolutions.com/api/uploads/documents',
-  BASE_URL_PUBLIC: 'https://api.desteksolutions.com/public',
+  baseUrl: 'http://app.kirandhurve.shop/api',
+  apiPrefix: 'http://app.kirandhurve.shop/api',
+  BASE_URL_IMAGE: 'http://app.kirandhurve.shop/api',
+  BASE_URL_DOCUMENTS: 'http://app.kirandhurve.shop/api/uploads/documents',
+  BASE_URL_PUBLIC: 'http://app.kirandhurve.shop/public',
   BASE_PATH_ASSETS: '/assets',
 };
 
