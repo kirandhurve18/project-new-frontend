d2e788fba3f598d97acd48b71f5a9ff1b7402d70
kirandhurve18 <kirand1895@gmail.com>
2026-01-29 16:38:37 +0530

    Update environment.prod.ts

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,10 +1,16 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api.desteksolutions.com/api',
-  apiPrefix: 'https://api.desteksolutions.com/api',
-  BASE_URL_IMAGE: 'https://api.desteksolutions.com/api',
-  BASE_URL_DOCUMENTS: 'https://api.desteksolutions.com/uploads/documents',
-  BASE_URL_PUBLIC: 'https://api.desteksolutions.com/public',
+  baseUrl: 'http://app.kirandhurve.shop/api',
+  apiPrefix: 'http://app.kirandhurve.shop/api',
+  BASE_URL_IMAGE: 'http://app.kirandhurve.shop/api',
+  BASE_URL_DOCUMENTS: 'http://app.kirandhurve.shop/uploads/documents',
+  BASE_URL_PUBLIC: 'http://app.kirandhurve.shop/public',
   BASE_PATH_ASSETS:'/assets',
 };
 
+ #baseUrl: 'https://api.desteksolutions.com/api',
+  #apiPrefix: 'https://api.desteksolutions.com/api',
+  #BASE_URL_IMAGE: 'https://api.desteksolutions.com/api',
+  #BASE_URL_DOCUMENTS: 'https://api.desteksolutions.com/uploads/documents',
+  #BASE_URL_PUBLIC: 'https://api.desteksolutions.com/public',
+  #BASE_PATH_ASSETS:'/assets',
